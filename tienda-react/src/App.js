a14ffa517f31d552bd9c0d087e1eb9a2ff291539
Lucila-Morelli <107009752+Lucila-Morelli@users.noreply.github.com>
2022-11-15 18:21:55 -0300

    carrito navegacion

--- tienda-react/src/App.js
+++ tienda-react/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 import State from './components/State/State';
+import ItemList from './components/ItemList/ItemList';
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import CartWidget from './components/CartWidget/CartWidget';
 
@@ -9,14 +10,14 @@ function App() {
   return (
 <>
 
-<ItemListContainer/>
-<State/>
+
 <BrowserRouter>
 <NavBar/> 
 <Routes>
   <Route path='/' element={<ItemListContainer/>}/>
   <Route path="ItemList" element= {<ItemList/>} />
   <Route path="cartWidget" element= {<CartWidget/>} />
+  
   </Routes>
 </BrowserRouter>
 </>

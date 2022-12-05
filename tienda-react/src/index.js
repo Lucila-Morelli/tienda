590329038e880092c88ce00efa4e21285fb0bc8f
Lucila-Morelli <107009752+Lucila-Morelli@users.noreply.github.com>
2022-12-05 17:07:09 -0300

    m

--- tienda-react/src/index.js
+++ tienda-react/src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUU7NBC7rwf54--aCFZtSQpORMzNgxLzw",
+  authDomain: "tienda-e8bce.firebaseapp.com",
+  projectId: "tienda-e8bce",
+  storageBucket: "tienda-e8bce.appspot.com",
+  messagingSenderId: "463197222161",
+  appId: "1:463197222161:web:57a6b56a142859436c88b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

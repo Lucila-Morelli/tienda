3a4103bc6e3c503fdab7f250d1d1347117ddf719
Lucila-Morelli <107009752+Lucila-Morelli@users.noreply.github.com>
2022-12-06 02:07:14 -0300

    firobase2

--- tienda-react/src/context/CartProvider.js
+++ tienda-react/src/context/CartProvider.js
@@ -5,6 +5,9 @@ export const cartContext = createContext([]);
 
 const CartProvider = ({children}) =>{
     const [cart, setCart] = useState([]);
+const isInCart = (id) => {
+    return cart.some((product) => product.id === id);
+};
 
     const addToCart = (product, quantity) =>{
         if(isInCart(product.id)){
